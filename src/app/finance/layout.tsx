d0ab472cc796b72bf870746009f3deaac3250ddc
ananthantharma <ID+ananthantharma@users.noreply.github.com>
2025-12-06 16:47:20 -0500

    Implement password manager features and update navigation

--- src/app/finance/layout.tsx
+++ src/app/finance/layout.tsx
@@ -1,10 +1,11 @@
+import { CurrencyDollarIcon, HomeIcon, Squares2X2Icon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import {redirect} from 'next/navigation';
-import {getServerSession} from 'next-auth';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
-import {authOptions} from '@/pages/api/auth/[...nextauth]';
+import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-export default async function FinanceLayout({children}: {children: React.ReactNode}) {
+export default async function FinanceLayout({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
@@ -23,14 +24,22 @@ export default async function FinanceLayout({children}: {children: React.ReactNo
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <Link
-                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white flex items-center gap-2"
+                    href="/">
+                    <HomeIcon className="h-5 w-5" />
+                    Home
+                  </Link>
+                  <Link
+                    className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white flex items-center gap-2"
                     href="/dashboard">
+                    <Squares2X2Icon className="h-5 w-5" />
                     Main Dashboard
                   </Link>
                   <Link
                     aria-current="page"
-                    className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+                    className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white flex items-center gap-2"
                     href="/finance">
+                    <CurrencyDollarIcon className="h-5 w-5" />
                     Finance
                   </Link>
                 </div>
