146fd9870d8cd6f5fd2e68d9cab2f39e84996f21
ananthantharma <ID+ananthantharma@users.noreply.github.com>
2025-12-24 11:52:47 -0500

    GridFS to file upload removed

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,4 +1,4 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 const options = {};
@@ -11,6 +11,10 @@ if (!uri) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
+// Replace the default database '/ADMIN?' with '/qt_portfolio?' to prevent
+// restricted access errors on Oracle Cloud's system database.
+const updatedUri = uri.replace('/ADMIN?', '/qt_portfolio?');
+
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
@@ -19,13 +23,13 @@ if (process.env.NODE_ENV === 'development') {
   };
 
   if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(updatedUri, options);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options);
+  client = new MongoClient(updatedUri, options);
   clientPromise = client.connect();
 }
 
