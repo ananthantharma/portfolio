13b7f592b6959f5f5f150cfe0cd95540ca77ab16
ananthantharma <ID+ananthantharma@users.noreply.github.com>
2025-12-26 22:23:33 -0500

    GridFS to file upload removed

--- src/app/api/debug/drive-status/route.ts
+++ src/app/api/debug/drive-status/route.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../../pages/api/auth/[...nextauth]';
+import { authOptions } from '../../../../pages/api/auth/[...nextauth]';
 import { getDriveClient } from '@/lib/googleDrive';
 import clientPromise from '@/lib/mongodb';
 
