c5b673a9cb040f79e2a5d22cd754c33303553d77
ananthantharma <ID+ananthantharma@users.noreply.github.com>
2025-12-26 21:16:40 -0500

    the one before this is Working model -------- i am trying to add logos for notes

--- src/components/Sections/Header.tsx
+++ src/components/Sections/Header.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable object-curly-spacing */
-import {Dialog, Transition} from '@headlessui/react';
+import { Dialog, Transition } from '@headlessui/react';
 import {
   Bars3BottomRightIcon,
   BookmarkIcon,
   ChatBubbleLeftRightIcon,
+  CloudIcon,
   CurrencyDollarIcon,
   DocumentTextIcon,
   HomeIcon,
@@ -12,10 +13,10 @@ import {
 } from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import Link from 'next/link';
-import {FC, Fragment, memo, useCallback, useMemo, useState} from 'react';
+import { FC, Fragment, memo, useCallback, useMemo, useState } from 'react';
 
-import {SectionId} from '../../data/data';
-import {useNavObserver} from '../../hooks/useNavObserver';
+import { SectionId } from '../../data/data';
+import { useNavObserver } from '../../hooks/useNavObserver';
 
 export const headerID = 'headerNav';
 
@@ -37,8 +38,8 @@ const Header: FC = memo(() => {
   );
 });
 
-const DesktopNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}> = memo(
-  ({navSections, currentSection}) => {
+const DesktopNav: FC<{ navSections: SectionId[]; currentSection: SectionId | null }> = memo(
+  ({ navSections, currentSection }) => {
     const baseClass =
       '-m-1.5 p-1.5 rounded-md font-bold first-letter:uppercase hover:transition-colors hover:duration-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-orange-500 sm:hover:text-orange-500 text-neutral-100';
     const activeClass = classNames(baseClass, 'text-orange-500');
@@ -68,6 +69,11 @@ const DesktopNav: FC<{navSections: SectionId[]; currentSection: SectionId | null
           <Link className={inactiveClass} href="/open">
             <span className="flex items-center gap-2">
               <ChatBubbleLeftRightIcon className="h-5 w-5" /> Open
+            </span>
+          </Link>
+          <Link className={inactiveClass} href="/drive">
+            <span className="flex items-center gap-2">
+              <CloudIcon className="h-5 w-5" /> Drive
             </span>
           </Link>
           <Link className={inactiveClass} href="/bookmarks">
@@ -101,8 +107,8 @@ const DesktopNav: FC<{navSections: SectionId[]; currentSection: SectionId | null
   },
 );
 
-const MobileNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}> = memo(
-  ({navSections, currentSection}) => {
+const MobileNav: FC<{ navSections: SectionId[]; currentSection: SectionId | null }> = memo(
+  ({ navSections, currentSection }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     const toggleOpen = useCallback(() => {
@@ -169,6 +175,11 @@ const MobileNav: FC<{navSections: SectionId[]; currentSection: SectionId | null}
                       <ChatBubbleLeftRightIcon className="h-5 w-5" /> Open
                     </span>
                   </Link>
+                  <Link className={inactiveClass} href="/drive" onClick={toggleOpen}>
+                    <span className="flex items-center gap-2">
+                      <CloudIcon className="h-5 w-5" /> Drive
+                    </span>
+                  </Link>
                   <Link className={inactiveClass} href="/bookmarks" onClick={toggleOpen}>
                     <span className="flex items-center gap-2">
                       <BookmarkIcon className="h-5 w-5" /> Bookmarks
@@ -210,7 +221,7 @@ const NavItem: FC<{
   activeClass: string;
   inactiveClass: string;
   onClick?: () => void;
-}> = memo(({section, current, inactiveClass, activeClass, onClick}) => {
+}> = memo(({ section, current, inactiveClass, activeClass, onClick }) => {
   return (
     <Link
       className={classNames(current ? activeClass : inactiveClass)}
